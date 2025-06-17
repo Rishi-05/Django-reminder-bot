79853ee1625ba6561af9362fd7e032946bb37ab1
Rishi Chilveri <115545106+Rishi-05@users.noreply.github.com>
2025-06-18 04:24:59 +0530

    Update request.py

--- request.py
+++ request.py
@@ -2,8 +2,8 @@ import requests
 
 # Step 1: Get the token
 response = requests.post("http://127.0.0.1:8000/api/token/", json={
-    "username": "Rishi",
-    "password": "Chilveri#rishi14"
+    "username": "your_own_registered_username",
+    "password": "password"
 })
 tokens = response.json()
 access = tokens['access']
